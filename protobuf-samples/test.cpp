2bff8c176cebecb62035f4bda02a0579d5a92c5a
Alireza67 <Alirezaabbasim@gmail.com>
2024-02-22 23:32:12 +0330

    [UPD] serialize_parse_partial_array
    [UPD] serialize_parse_partial_array

--- protobuf-samples/test.cpp
+++ protobuf-samples/test.cpp
@@ -151,3 +151,29 @@ TEST(protobuf, serialize_parse_str)
 	auto result = address_book2.DebugString();
 	EXPECT_EQ(main, result);
 }
+
+TEST(protobuf, serialize_parse_partial_array)
+{
+	AddressBook address_book;
+
+	Person* person = address_book.add_people();
+	person->set_name("john doe");
+	person->set_id(12345);
+	person->set_email("john.doe@example.com");
+
+	person = address_book.add_people();
+	person->set_name("david adamz");
+	person->set_id(54321);
+	person->set_email("david.adamz@example.com");
+
+	
+	std::vector<char> data(address_book.ByteSize(), ' ');
+	auto res = address_book.SerializePartialToArray(data.data(), data.size());
+
+	AddressBook address_book2;
+	address_book2.ParsePartialFromArray(data.data(), data.size());
+
+	auto main = address_book.DebugString();
+	auto result = address_book2.DebugString();
+	EXPECT_EQ(main, result);
+}
